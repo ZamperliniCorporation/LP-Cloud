2a25bafc4b27b16f6a6f43d2179fb55343623bda
dinbruno <brunodinosantos@Outlook.com>
2025-06-02 15:21:17 -0300

    Add Google Tag Manager integration to layout.tsx for enhanced tracking capabilities and analytics support.

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,10 +20,31 @@ export default function RootLayout({
   return (
     <html lang="pt-BR">
       <head>
+        {/* Google Tag Manager */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-WKPDKW76');`,
+          }}
+        />
+        {/* End Google Tag Manager */}
         <GoogleAnalytics />
         <GoogleTagManager />
       </head>
       <body className={`${ltWave.variable} dark-transition`} suppressHydrationWarning>
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-WKPDKW76"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
+        {/* End Google Tag Manager (noscript) */}
         <ThemeProvider>
           <LanguageProvider>{children}</LanguageProvider>
         </ThemeProvider>
